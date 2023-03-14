52540ddef3e6f5cae377b81a370cfc0240e1c45c
nouhaila-samih <nouhaila.samih21@hotmail.com>
2023-03-14 16:27:25 +0100

    f

--- Backend/server.js
+++ Backend/server.js
@@ -23,19 +23,11 @@ app.use('/api/message', messageRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname1, '/frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running..');
-  });
-}
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://N-Chat.onrender.com"],
+  })
+)
 
 // --------------------------deployment------------------------------
 
